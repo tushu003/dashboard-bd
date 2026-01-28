49fb2d179e502ee38eb5ca8e7ec1fc45bd280cb4
tushu003 <tahminata2015@gmail.com>
2026-01-28 17:57:53 +0600

    modify navbar

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -12,7 +12,7 @@ interface NavbarProps {
 
 export default function Navbar({ title, onMenuClick }: NavbarProps) {
   return (
-    <header className="fixed top-0 left-0 lg:left-64 right-0 z-50 flex h-16 items-center justify-between border-b border-border bg-[#111B3C] px-4 md:px-6">
+    <header className="fixed top-0 left-0 lg:left-64 right-0 z-50 flex h-16 items-center justify-between bg-[#111B3C] px-4 md:px-6">
       <div className="flex items-center gap-4">
         <Button
           variant="ghost"
--- components/common/sidebar.tsx
+++ components/common/sidebar.tsx
@@ -47,7 +47,7 @@ export default function Sidebar({ isOpen = false, onClose }: SidebarProps) {
 
   return (
     <>
-      {/* Mobile Overlay */}
+      {/* Mobile */}
       {isOpen && (
         <div
           className="fixed inset-0 z-40 bg-black/50 lg:hidden"
@@ -64,7 +64,7 @@ export default function Sidebar({ isOpen = false, onClose }: SidebarProps) {
       >
         <div className="flex h-full flex-col">
           {/* Logo */}
-          <div className="flex h-16 items-center justify-between border-b border-border px-6">
+          <div className="flex h-16 items-center justify-between border-border px-6">
             <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-green-500">
               <Zap className="h-6 w-6 text-white" />
             </div>
